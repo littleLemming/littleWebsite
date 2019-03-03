8132647db378eba9045040fa563eeb25d7f6d4c6
littleLemming <lemming0061@gmail.com>
2019-03-03 19:47:28 +0100

    let's try this factory

--- app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask("littleWebsite")
-
-@app.route("/")
-def hello():
-    return "Hello MOTHAFUCKING World!!!!"
---
+++ modules/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask('littleWebsite', instance_relative_config=True)
+
+    # here the db should be added
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # first page - just for testing
+    @app.route('/hello')
+    def hello():
+        return 'HELLO WORLD'
+
+    return app
