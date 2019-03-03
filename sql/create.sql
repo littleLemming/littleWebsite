2b23b3eb0a3905068eca303e68f53c613e5efa55
cyborg00222 <peter@kowalsky.at>
2019-03-03 18:25:16 +0000

    added example pages to sql files

--- sql/create.sql
+++ sql/create.sql
@@ -9,3 +9,12 @@ CREATE TABLE IF NOT EXISTS pages (
     INDEX(route),
     FULLTEXT(markdown)
 )  ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    group_id TINYINT NOT NULL,
+    PRIMARY KEY (user_id),
+    INDEX(username)
+) ENGINE=INNODB;
---
+++ sql/example_pages.sql
@@ -0,0 +1,7 @@
+insert into pages (title, route, date, status, markdown)
+VALUES
+("lemming.dev", "/index", NOW(), 1, "##Hello world!");
+
+insert into pages (title, route, date, status, markdown)
+VALUES
+("about me...", "/about-me", NOW(), 0, "##Not published, yet! :D");
